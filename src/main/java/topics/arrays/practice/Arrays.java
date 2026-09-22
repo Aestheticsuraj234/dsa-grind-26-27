37929b121fe2406dd412c59a99c4ad2f6239828c
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-09-22 21:58:14 +0530

    Remove commented-out code in Arrays.java to enhance readability and maintainability; streamline the main method structure.

--- src/main/java/topics/arrays/practice/Arrays.java
+++ src/main/java/topics/arrays/practice/Arrays.java
@@ -1,41 +1,73 @@
 package topics.arrays.practice;
 
-import java.util.Scanner;
-
 public class Arrays {
+
     public static void main(String[] args) {
+
+
+      // int arr[] = {12,14,1245};
+      // int n = arr.length;
+
+      // int sum = 0;
+
+      // for(int i =0;i<n;i++){
+      //    sum += arr[i];
+      //    System.out.println(sum);
+      // }
+
+      // int arr[]={2,3,10,20};
+
+      // int ans = 1;
+
+      // int n = arr.length;
+
+      // // int ans = 1
+
+      // for(int i = 0 ;i<n;i++){
+      //    int value = arr[i];
+      //    ans = ans * value;
+      //    System.out.println(ans);
+      // }
+
         // int arr[];
 
         // arr = new int[5];
-
         // int brr[] = {1, 2, 3, 4, 5};
-
         // System.out.println("Value at 0 index ");
         // System.out.println(brr[0]);
         // System.out.println(brr[1]);
         // System.out.println(brr[2]);
         // System.out.println(brr[3]);
-        
-
         // for(int i = 0; i <brr.length -1; i++){
         //     System.out.println(brr[i]);
         // }
-
         // for(int val:brr){
         //     System.out.println(val);
-        // }
+      //   // }
+      //   int arr[] = new int[5];
 
-        int arr[] = new int[5];
+      //   Scanner sc = new Scanner(System.in);
+      //   for (int i = 0; i <= arr.length - 1; i++) {
+      //       System.out.println("Enter the value at " + i + " index");
+      //       arr[i] = sc.nextInt();
+      //   }
 
-        Scanner sc = new Scanner(System.in);
- for(int i =0; i<=arr.length -1; i++){
-    System.out.println("Enter the value at " + i + " index");
-    arr[i] = sc.nextInt();
- }
+      //   for (int val : arr) {
+      //       System.out.println(val);
+      //   }
 
- for(int val:arr){
-    System.out.println(val);
- }
-        
+      // int arr[] = {3,2,-5,21,10};
+
+      // int n = arr.length;
+
+      // int maxValue = arr[0];
+
+      // for(int i =0;i<=n-1;i++){
+      //    if(arr[i]>maxValue){
+      //       maxValue = arr[i];
+      //    }
+      // }
+
+      // System.out.println(maxValue);
     }
 }
